d8a2dc61e0b36ddf67a728ef1c8840b583c1f783
tliz <tlizzen@gmail.com>
2015-02-23 19:47:36 +0300

    assign 01 23022015

---
+++ Documents/Projects/Python/assignment1/assign1.py
@@ -0,0 +1,28 @@
+#Function that takes parameter and prints that string parameter
+
+def string_printer(string_parameter):
+	return string_parameter
+
+print string_printer("Wopa Cohort 3")
+
+#Function that takes a string parameter and returns the string that contains hello and the string parameter
+
+def hello_string_printer(string_parameter):
+		return "hello" + " " + string_parameter
+
+print hello_string_printer("hi")
+
+
+#a function that allows the user to enter 2 numbers and returns a sum of the two numbers
+
+def sum_of_two_numbers(a, b):
+	return a +b
+
+print sum_of_two_numbers(3,7)
+
+# Program that allow a user to enter two numbers and returns the division of these two numbers
+
+def div_of_two_numbers(a,y):
+		return a/y
+
+print div_of_two_numbers(6,3)
